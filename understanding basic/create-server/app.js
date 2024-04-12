aa5a9f7071635fe1e34cf8592f13722008bff36e
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-12 10:40:11 +0300

    Update app.js

--- understanding basic/create-server/app.js
+++ understanding basic/create-server/app.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 
 const server=http.createServer((req,res)=>{
-    console.log(req)
+    console.log(req.url,req.method,req.headers)
+    // process.exit()
 });
 server.listen(3000);
