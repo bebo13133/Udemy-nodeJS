7f8762ea5c0bb0f08aeef388047a5ff220483589
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-13 18:00:08 +0300

    loop , event , routes

---
+++ understanding basic/02-parsing-request-bodies/app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const routes = require('./routes');
+const server = http.createServer((req, res) => {
+
+});
+
+server.listen(3000);
--- understanding basic/create-server/app.js
+++ understanding basic/create-server/app.js
@@ -2,7 +2,13 @@ const http = require('http')
 
 
 const server=http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers)
+    // console.log(req.url,req.method,req.headers)
     // process.exit()
+    const url = req.url
+    if(url === '/'){
+                                
+    }
+res.setHeader('Content-Type', 'text/html')
+
 });
 server.listen(3000);
