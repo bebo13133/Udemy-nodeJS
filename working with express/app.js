844ece5f5e404bd076c94d8db83ee575033e5a55
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-16 13:35:46 +0300

    Update app.js

--- working with express/app.js
+++ working with express/app.js
@@ -2,13 +2,14 @@ const http = require('http');
 const express = require('express');
 const app= express();
 
-app.use((req,res,next)=> {
-    console.log("In middleware!")
-    next();
-})
-app.use((req,res,next)=> {
+app.use('/add-product',(req,res,next)=> {
     console.log("In another middleware!")
+    res.send('<h1The "Add product"</h1>')
 })
-const server = http.createServer(app);
+app.use('/',(req,res,next)=> {
+    console.log("In another middleware!")
+    res.send('<h1>Hello from express</h1>')
+})
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
