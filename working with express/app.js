db5aba7dd0b5492612e385977eabb6b3583c03f1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-17 18:09:30 +0300

    add

--- working with express/app.js
+++ working with express/app.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const express = require('express');
 const app= express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended:false}))
 
 app.use('/add-product',(req,res,next)=> {
-    console.log("In another middleware!")
-    res.send('<h1The "Add product"</h1>')
+    // console.log("In another middleware!")
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product<button/><form/>')
+})
+app.use('/product',(req,res,next)=> {
+    console.log(req.body)
+    res.redirect('/')
+    // res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product<button/><form/>')
 })
 app.use('/',(req,res,next)=> {
-    console.log("In another middleware!")
+    // console.log("In another middleware!")
     res.send('<h1>Hello from express</h1>')
 })
 // const server = http.createServer(app);
