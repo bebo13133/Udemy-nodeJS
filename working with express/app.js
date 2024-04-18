89ae95b22e91a4fb56b860dfc9b4a58deba3b709
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-18 11:36:33 +0300

    add

--- working with express/app.js
+++ working with express/app.js
@@ -7,6 +7,7 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended:false}))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(shopRoutes);
 
 app.use('/admin',adminRoutes);
