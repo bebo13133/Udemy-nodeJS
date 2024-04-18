92e5ba0af89af7fcbd8bc53c0ac243ae04b003d9
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-18 11:27:10 +0300

    add css style

--- working with express/routes/admin.js
+++ working with express/routes/admin.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-
+const rootDir = require('../util/path')
 router.get('/add-product',(req,res,next)=> {
     // console.log("In another middleware!")
-    res.sendFile(path.join(__dirname,'../','views', 'add-product.html'));
+    res.sendFile(path.join(rootDir,'views', 'add-product.html'));
 })
 router.post('/add-product',(req,res,next)=> {
     console.log(req.body)
--- working with express/routes/shop.js
+++ working with express/routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const rootDir = require('../util/path')
 router.get('/',(req,res,next)=> {
     // console.log("In another middleware!")
-    res.sendFile(path.join(__dirname,'../','views','shop.html'))
+    res.sendFile(path.join(rootDir,'views','shop.html'))
 })
 
 module.exports = router
