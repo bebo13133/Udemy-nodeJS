c708d859ac2a365f133526f7c61f18a3a1562551
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-22 09:27:40 +0300

    add dynamic shop and products

--- Dynamic content and templating engines/routes/admin.js
+++ Dynamic content and templating engines/routes/admin.js
@@ -7,7 +7,12 @@ const products =[]
 
 router.get('/add-product',(req,res,next)=> {
     // console.log("In another middleware!")
-    res.render('add-product', {pageTitle: 'Add Product', path:'/admin/add-product'})
+    res.render('add-product', {pageTitle: 'Add Product',
+     path:'/admin/add-product',
+     activeAddProduct:true,
+     formsCSS:true,
+     productCSS:true
+    })
 })
 router.post('/add-product',(req,res,next)=> {
     products.push({title: req.body.title})
--- Dynamic content and templating engines/routes/shop.js
+++ Dynamic content and templating engines/routes/shop.js
@@ -6,7 +6,13 @@ const adminData = require('./admin')
 router.get('/',(req,res,next)=> {
     // console.log("In another middleware!")
     const products = adminData.products
-res.render('shop', {prods:products,pageTitle:'Shop', path:'/', hasProducts: products.length>0})
+res.render('shop', {
+    prods:products,pageTitle:'Shop', 
+    path:'/', 
+    hasProducts: products.length>0,
+    activeShop:true,
+    productCSS:true
+})
 })
 
 module.exports = router
