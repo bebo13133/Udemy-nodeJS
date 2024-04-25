397323452bef451d4caeeea2af9ab1c503fc7dff
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-25 14:24:48 +0300

    add

--- enhancing app/routes/admin.js
+++ enhancing app/routes/admin.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
-router.get('/products');
+router.get('/products',adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
