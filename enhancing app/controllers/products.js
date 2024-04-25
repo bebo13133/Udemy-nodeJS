f3b4be2ebb06296bcd95c8158cf3fb301513cc99
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-25 08:59:58 +0300

    add views

--- enhancing app/controllers/products.js
+++ enhancing app/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -18,9 +18,9 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Shop',
+      pageTitle: 'Product list',
       path: '/',
       hasProducts: products.length > 0,
       activeShop: true,
