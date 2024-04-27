b022170cf5402bf22eadf81ee76ef9061fdc24f2
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-04-27 08:10:35 +0300

    add orders

--- enhancing app/controllers/shop.js
+++ enhancing app/controllers/shop.js
@@ -26,6 +26,12 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
--- enhancing app/routes/shop.js
+++ enhancing app/routes/shop.js
@@ -11,6 +11,7 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
