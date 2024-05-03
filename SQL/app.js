d0c9cd737a7d4b9b450762fa229b29c24467fb68
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-03 09:53:22 +0300

    Update app.js

--- SQL/app.js
+++ SQL/app.js
@@ -15,7 +15,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 db.execute('SELECT * FROM products')
 .then((result)=>{
-console.log(result)
+console.log(result[0],result[1])
 }).catch((err)=>{
 console.log(err.message);
 })
