2f1788a9f3681ae4bdd58b41dc4cdf048742aa6f
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-04 12:21:22 +0300

    Update database.js

--- Sequelise/util/database.js
+++ Sequelise/util/database.js
@@ -1,10 +1,4 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-
-const  pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database:'node-complete',
-    password: 'bebo840512'
-})
-module.exports = pool.promise()
+const sequelize= new Sequelize('node-complete', 'root', 'bebo840512',{dialect:'mysql',host:'localhost'});
+module.exports = sequelize;
