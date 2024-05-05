02e3729e2949026790fa65aef88fb19824632250
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-05 08:59:53 +0300

    Update models

--- Sequelise/models/product.js
+++ Sequelise/models/product.js
@@ -1,30 +1,16 @@
-const db = require('../util/database')
-const Cart = require('./cart');
+const Sequelize = require('sequelize')
 
+const sequelize= require('../util/database')
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true 
+  },
+  title:Sequelize.STRING,
+  price:{
+
   }
-
-  save() {
-    return db.execute('INSERT INTO products ( title,price, imageUrl, description) VALUES(?,?,?,?)',
-    [this.title,this.price, this.imageUrl, this.description])
-  }
-
-  static deleteById(id) {
- 
-  }
-
-  static fetchAll() {
-   return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id) {
-  return db.execute('SELECT * FROM products WHERE products.id = ? ',[id])
-  }
-};
+})
