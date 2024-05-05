79ef4e113d77f2a85d52449f21b0d77b74b69a96
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-05 09:55:26 +0300

    Update shop.js

--- Sequelise/controllers/shop.js
+++ Sequelise/controllers/shop.js
@@ -2,18 +2,15 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData])=>{
+
+  Product.findAll().then(products=>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  })
-  .catch(err => {
-    console.log(err)
-  });
+  }).catch(err=>console.log(err))
 
-   
 };
 
 exports.getProduct = (req, res, next) => {
@@ -30,7 +27,7 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll(),then(products=>{
+  Product.findAll().then(products=>{
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
