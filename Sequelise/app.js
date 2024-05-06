6be5698f8a02236eef1483f0dc9886ff82b1eee2
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-06 09:28:34 +0300

    add

--- Sequelise/app.js
+++ Sequelise/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,7 +24,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result=>{
+Product.belongsTo(User,{constraints:true, onDelete:'CASCADE'});
+User.hasMany(Product);
+
+sequelize.sync({force:true}).then(result=>{
     // console.log(result)
     app.listen(3000);
 
