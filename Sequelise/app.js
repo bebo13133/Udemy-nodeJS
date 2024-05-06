999001104a1a0031f926cff296d4a96dfb4deb0f
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-06 11:02:24 +0300

    Update app.js

--- Sequelise/app.js
+++ Sequelise/app.js
@@ -11,7 +11,7 @@ const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', 'view ');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -19,7 +19,9 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req,res,next)=>{
-    User.findByPk(1)
+    User.findByPk(1).then(user=>{
+        req.user=user
+    }).catch((err)=>{console.log(err)});
 })
 
 app.use('/admin', adminRoutes);
