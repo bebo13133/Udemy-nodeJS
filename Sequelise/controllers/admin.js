c7520ec51266eb3f97d1b676ee5473eac9aa2dd1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-06 08:45:50 +0300

    Update admin.js

--- Sequelise/controllers/admin.js
+++ Sequelise/controllers/admin.js
@@ -31,7 +31,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId).then(product => {
     if (!product) {
       return res.redirect('/');
     }
@@ -41,7 +41,7 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
-  });
+  }).catch(err => {console.log(err)})
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -50,26 +50,32 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
+ Product.findByPk(prodId)
+ .then(product=>{
+  product.title = updatedTitle;
+  product.description = updatedDesc;
+  product.price = updatedPrice;
+  product.imageUrl = updatedImageUrl;
+  return product.save()
+ })
+ .then(result=>{
+  console.log('UPDATE PRODUCT')
+ })
+ .catch(err=>console.log(err))
   res.redirect('/admin/products');
 };
-
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
+
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
