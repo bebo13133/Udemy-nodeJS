3f087c1e82e5dcfcf1e633d86a30bd79c65ef36e
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-05-08 18:31:06 +0300

    Update database.js

--- Sequelise/util/database.js
+++ Sequelise/util/database.js
@@ -3,7 +3,5 @@
 const Sequelize = require('sequelize');
 
 const sequelize= new Sequelize('node-complete', 'root', 'bebo840512',{dialect:'mysql',host:'localhost'});
-
-
 module.exports = sequelize;
 
